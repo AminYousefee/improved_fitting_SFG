169b97a63ab45ccbb1627a307cb3143d9e66cfc0
amin <m.amin.yousefii@gmail.com>
2023-06-13 00:06:44 -0700

    commit

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 As = [40, 70, 30, -50, 50, -40]
-NR = 10
+NR = 15
 peak_positions = [2960, 3000, 3030, 3050, 3100, 3110]
 gamma = 10
 
@@ -10,7 +11,7 @@ def add_noise_to_array(arr, percentage):
     arr_max = np.max(arr)
     arr_min = np.min(arr)
     max_noise = abs((arr_max - arr_min) * percentage / 100)
-    noise = np.random.normal(0, max_noise, arr.shape)
+    noise = np.abs(np.random.normal(0, max_noise, arr.shape))
     return arr + noise
 
 def func(omega):
@@ -23,6 +24,7 @@ def func(omega):
 x = np.arange(2800, 3300, 5)
 y = np.vectorize(func)(x)
 y2 = add_noise_to_array(y, 5)
+print(tabulate(np.array([x, y, y2]).T))
 fig, ax = plt.subplots()
 ax.plot(x, y2)
 extra_index = 0
